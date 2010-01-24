2a724998a846a2eb7d3563eecec21c1623c77f76
Matt Sanford <matt@twitter.com>
2010-01-24 12:36:02 -0800

    Update javadocs

--- src/com/twitter/Extractor.java
+++ src/com/twitter/Extractor.java
@@ -4,11 +4,23 @@ package com.twitter;
 import java.util.*;
 import java.util.regex.*;
 
+/**
+ * A class to extract usernames, lists, hashtags and URLs from Tweet text.
+ */
 public class Extractor {
 
+  /**
+   * Create a new extractor.
+   */
   public Extractor() {
   }
 
+  /**
+   * Extract @username references from Tweet text. A mention is an occurance of @username anywhere in a Tweet.
+   *
+   * @param text of the tweet from which to extract usernames
+   * @return List of usernames referenced (without the leading @ sign)
+   */
   public List<String> extractMentionedScreennames(String text) {
     if (text == null) {
       return null;
@@ -17,6 +29,13 @@ public class Extractor {
     return extractList(Regex.EXTRACT_MENTIONS, text, Regex.EXTRACT_MENTIONS_GROUP_USERNAME);
   }
 
+  /**
+   * Extract a @username reference from the beginning of Tweet text. A reply is an occurance of @username at the
+   * beginning of a Tweet, preceded by 0 or more spaces.
+   *
+   * @param text of the tweet from which to extract the replied to username
+   * @return username referenced, if any (without the leading @ sign). Returns null if this is not a reply.
+   */
   public String extractReplyScreenname(String text) {
     if (text == null) {
       return null;
@@ -30,6 +49,12 @@ public class Extractor {
     }
   }
 
+  /**
+   * Extract URL references from Tweet text.
+   *
+   * @param text of the tweet from which to extract URLs
+   * @return List of URLs referenced.
+   */
   public List<String> extractURLs(String text) {
     if (text == null) {
       return null;
@@ -38,6 +63,12 @@ public class Extractor {
     return extractList(Regex.VALID_URL, text, Regex.VALID_URL_GROUP_URL);
   }
 
+  /**
+   * Extract #hashtag references from Tweet text.
+   *
+   * @param text of the tweet from which to extract hashtags
+   * @return List of hashtags referenced (without the leading # sign)
+   */
   public List<String> extractHashtags(String text) {
     if (text == null) {
       return null;
@@ -46,6 +77,14 @@ public class Extractor {
     return extractList(Regex.AUTO_LINK_HASHTAGS, text, Regex.AUTO_LINK_HASHTAGS_GROUP_TAG);
   }
 
+  /**
+   * Helper method for extracting multiple matches from Tweet text.
+   *
+   * @param pattern to match and use for extraction
+   * @param text of the Tweet to extract from
+   * @param groupNumber the capturing group of the pattern that should be added to the list.
+   * @return list of extracted values, or an empty list if there were none.
+   */
   private List<String> extractList(Pattern pattern, String text, int groupNumber) {
     List<String> extracted = new ArrayList<String>();
     Matcher matcher = pattern.matcher(text);
