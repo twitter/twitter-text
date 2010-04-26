17f1f0a7e0e40109c07c6af3c36c88c1abebe246
Matt Sanford <matt@twitter.com>
2010-04-26 11:37:25 -0700

    Brittspace fix. Make tag name optional. Improve rdoc on hit highlighter. Add more tests (some failing)

--- lib/hithighlighter.rb
+++ lib/hithighlighter.rb
@@ -5,21 +5,27 @@ module Twitter
   module HitHighlighter
     # Default Tag used for hit highlighting
     DEFAULT_HIGHLIGHT_TAG = "em"
-    
+
     # Add <tt><em></em></tt> tags around the <tt>hits</tt> provided in the <tt>text</tt>. The
     # <tt>hits</tt> should be an array of (start, end) index pairs, relative to the original
     # text, before auto-linking (but the <tt>text</tt> may already be auto-linked if desired)
-    def hit_highlight(text, hits = [])
+    #
+    # The <tt><em></em></tt> tags can be overridden using the <tt>:tag</tt> option. For example:
+    #
+    #  irb> hit_highlight("test hit here", [[5, 8]], :tag => 'strong')
+    #  => "test <strong>hit</strong> here"
+    def hit_highlight(text, hits = [], options = {})
       if hits.empty?
         return text
       end
-      
+
+      tag_name = options[:tag] || DEFAULT_HIGHLIGHT_TAG
+      tags = ["<" + tag_name + ">", "</" + tag_name + ">"]
+
       chunks = text.split("<").map do |item|
         item.blank? ? item : item.split(">")
       end.flatten
 
-      tags = ["<" + DEFAULT_HIGHLIGHT_TAG + ">", "</" + DEFAULT_HIGHLIGHT_TAG + ">"]
-      
       result = ""
       chunk_index, chunk = 0, chunks[0]
       prev_chunks_len = 0
@@ -27,10 +33,10 @@ module Twitter
       start_in_chunk = false
       for hit, index in hits.flatten.each_with_index do
         tag = tags[index % 2]
-        
+
         placed = false
         until chunk.nil? || hit < prev_chunks_len + chunk.length do
-          result << chunk[chunk_cursor..-1] 
+          result << chunk[chunk_cursor..-1]
           if start_in_chunk && hit == prev_chunks_len + chunk.length
             result << tag
             placed = true
@@ -42,7 +48,7 @@ module Twitter
           chunk = chunks[chunk_index]
           start_in_chunk = false
         end
-        
+
         if !placed && !chunk.nil?
           hit_spot = hit - prev_chunks_len
           result << chunk[chunk_cursor...hit_spot] + tag
@@ -52,14 +58,14 @@ module Twitter
           end
         end
       end
-      
+
       if !chunk.nil?
         result << chunk[chunk_cursor..-1]
         for index in chunk_index+1..chunks.length-1
           result << (index.even? ? chunks[index] : "<#{chunks[index]}>")
         end
       end
-      
+
       result
     end
   end
