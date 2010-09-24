e49e82d86bec7a8dd9e9f3171266825a9f0b55af
Matt Sanford <matt@twitter.com>
2010-09-24 08:57:52 -0700

    Update to the latest conformance data. Handle the compressed old-style RT autolink case

--- src/com/twitter/Regex.java
+++ src/com/twitter/Regex.java
@@ -57,7 +57,7 @@ public class Regex {
   public static final int AUTO_LINK_HASHTAGS_GROUP_HASH = 2;
   public static final int AUTO_LINK_HASHTAGS_GROUP_TAG = 3;
 
-  public static final Pattern AUTO_LINK_USERNAMES_OR_LISTS = Pattern.compile("([^a-z0-9_]|^)(" + AT_SIGNS + "+)([a-z0-9_]{1,20})(/[a-z][a-z0-9_\\-\\x80-\\xFF-]{0,24})?($|.)", Pattern.CASE_INSENSITIVE);
+  public static final Pattern AUTO_LINK_USERNAMES_OR_LISTS = Pattern.compile("([^a-z0-9_]|^|RT:?)(" + AT_SIGNS + "+)([a-z0-9_]{1,20})(/[a-z][a-z0-9_\\-\\x80-\\xFF-]{0,24})?($|.)", Pattern.CASE_INSENSITIVE);
   public static final int AUTO_LINK_USERNAME_OR_LISTS_GROUP_BEFORE = 1;
   public static final int AUTO_LINK_USERNAME_OR_LISTS_GROUP_AT = 2;
   public static final int AUTO_LINK_USERNAME_OR_LISTS_GROUP_USERNAME = 3;
