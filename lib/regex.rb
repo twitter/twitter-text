b6a8310af250652509e00bd10fe5aad694ae741b
J.P. Cummins <jcummins@twitter.com>
2011-03-25 15:33:50 -0700

    Updated domain regex to only allow underscores in subdomains Updated Rakefile to include rubygems

--- lib/regex.rb
+++ lib/regex.rb
@@ -54,7 +54,10 @@ module Twitter
 
     # URL related hash regex collection
     REGEXEN[:valid_preceding_chars] = /(?:[^-\/"':!=A-Z0-9_@＠]|^|\:)/i
-    REGEXEN[:valid_domain] = /(?:[^[:punct:]\s][\._-](?=[^[:punct:]\s])|[^[:punct:]\s]){1,}\.[a-z]{2,}(?::[0-9]+)?/i
+
+    REGEXEN[:valid_subdomain] = /([^[:punct:]\s]([_-]|[^[:punct:]\s])*)?[^[:punct:]\s]\./
+    REGEXEN[:valid_domain_name] = /([^[:punct:]\s]([-]|[^[:punct:]\s])*)?[^[:punct:]\s]/
+    REGEXEN[:valid_domain] = /#{REGEXEN[:valid_subdomain]}*#{REGEXEN[:valid_domain_name]}\.[a-z]{2,}(?::[0-9]+)?/i
 
     REGEXEN[:valid_general_url_path_chars] = /[a-z0-9!\*';:=\+\,\$\/%#\[\]\-_~]/i
     # Allow URL paths to contain balanced parens
--- spec/test_urls.rb
+++ spec/test_urls.rb
@@ -22,6 +22,10 @@ module TestUrls
     "http://x.com/has/one/char/domain",
     "http://t.co/nwcLTFF",
     "http://sub_domain-dash.twitter.com",
+    "http://a.b.cd",
+    "http://a_b.c-d.com",
+    "http://a-b.b.com",
+    "http://twitter-dash.com",
     # "t.co/nwcLTFF"
   ] unless defined?(TestUrls::VALID)
 
@@ -30,7 +34,16 @@ module TestUrls
     "http://tld-too-short.x",
     "www.foobar.com",
     "WWW.FOOBAR.COM",
-    "http://-doman_dash.com"
+    "http://-doman_dash.com",
+    "http://_leadingunderscore.twitter.com",
+    "http://trailingunderscore_.twitter.com",
+    "http://-leadingdash.twitter.com",
+    "http://trailingdash-.twitter.com",
+    "http://-leadingdash.com",
+    "http://trailingdash-.com",
+    "http://no_underscores.com",
+    "http://test.c_o_m",
+    "http://test.c-o-m"
   ] unless defined?(TestUrls::INVALID)
 
 end
