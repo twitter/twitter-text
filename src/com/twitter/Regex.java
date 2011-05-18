4276941a943c6d7d6192697c236213a29520cc85
Matt Sanford <matt@twitter.com>
2011-05-18 16:04:39 -0700

    Add support for Russian & Korean hashtags as well as new URL conformance tests

--- src/com/twitter/Regex.java
+++ src/com/twitter/Regex.java
@@ -10,13 +10,24 @@ public class Regex {
   "with_friend","with_friends","statuses","status","activity","favourites",
   "favourite","favorite","favorites"};
 
-  private static final String HASHTAG_CHARACTERS = "[a-z0-9_\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff]";
+  private static String LATIN_ACCENTS_CHARS = "\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff";
+  private static final String HASHTAG_ALPHA_NUMERIC = "[a-z0-9_" + LATIN_ACCENTS_CHARS +
+                                                   "\\u0400-\\u04ff0\\u0500-\\u0527" + // Cyrillic
+                                                   "\\u1100-\\u11ff\\u3130-\\u3185\\uA960-\\uA97F\\uAC00-\\uD7AF\\uD7B0-\\uD7FF" + // Hangul (Korean)
+                                                   "]";
+  private static final String HASHTAG_ALPHA = "[a-z" + LATIN_ACCENTS_CHARS +
+                                                   "\\u0400-\\u04ff0\\u0500-\\u0527" + // Cyrillic
+                                                   "\\u1100-\\u11ff\\u3130-\\u3185\\uA960-\\uA97F\\uAC00-\\uD7AF\\uD7B0-\\uD7FF" + // Hangul (Korean)
+                                                   "]";
 
   /* URL related hash regex collection */
   private static final String URL_VALID_PRECEEDING_CHARS = "(?:[^\\-/\"':!=A-Z0-9_@＠]+|^|\\:)";
-  private static final String URL_VALID_DOMAIN = "(?:[^\\p{Punct}\\s][\\.-](?=[^\\p{Punct}\\s])|[^\\p{Punct}\\s]){1,}\\.[a-z]{2,}(?::[0-9]+)?";
 
-  private static final String URL_VALID_GENERAL_PATH_CHARS = "[a-z0-9!\\*';:=\\+\\$/%#\\[\\]\\-_,~]";
+  private static final String URL_VALID_SUBDOMAIN = "(?:(?:[^\\p{Punct}\\s](?:[_-]|[^\\p{Punct}\\s])*)?[^\\p{Punct}\\s]\\.)";
+  private static final String URL_VALID_DOMAIN_NAME = "(?:[^\\p{Punct}\\s](?:[-]|[^\\p{Punct}\\s])*)?[^\\p{Punct}\\s]";
+  private static final String URL_VALID_DOMAIN = URL_VALID_SUBDOMAIN + "*" + URL_VALID_DOMAIN_NAME + "\\.[a-z]{2,}(?::[0-9]+)?";
+
+  private static final String URL_VALID_GENERAL_PATH_CHARS = "[a-z0-9!\\*';:=\\+\\$/%#\\[\\]\\-_,~\\.\\|]";
   /** Allow URL paths to contain balanced parens
    *  1. Used in Wikipedia URLs like /Primer_(film)
    *  2. Used in IIS sessions like /S(dfd346)/
@@ -32,7 +43,7 @@ public class Regex {
    *   2. Allow =&# for empty URL parameters and other URL-join artifacts
   **/
   private static final String URL_VALID_URL_PATH_ENDING_CHARS = "(?:[a-z0-9=_#/\\-\\+]+|"+URL_BALANCE_PARENS+")";
-  private static final String URL_VALID_URL_QUERY_CHARS = "[a-z0-9!\\*'\\(\\);:&=\\+\\$/%#\\[\\]\\-_\\.,~]";
+  private static final String URL_VALID_URL_QUERY_CHARS = "[a-z0-9!\\*'\\(\\);:&=\\+\\$/%#\\[\\]\\-_\\.,~\\|]";
   private static final String URL_VALID_URL_QUERY_ENDING_CHARS = "[a-z0-9_&=#/]";
   private static final String VALID_URL_PATTERN_STRING =
   "(" +                                                            //  $1 total match
@@ -55,8 +66,6 @@ public class Regex {
   ")";
 
   private static String AT_SIGNS_CHARS = "@\uFF20";
-  private static String LATIN_ACCENTS_CHARS = "\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff";
-
 
 
   /* Begin public constants */
@@ -64,7 +73,7 @@ public class Regex {
 
   public static final Pattern SCREEN_NAME_MATCH_END = Pattern.compile("^(?:[" + AT_SIGNS_CHARS + LATIN_ACCENTS_CHARS + "]|://)");
 
-  public static final Pattern AUTO_LINK_HASHTAGS = Pattern.compile("(^|[^0-9A-Z&/]+)(#|\uFF03)([0-9A-Z_]*[A-Z_]+" + HASHTAG_CHARACTERS + "*)", Pattern.CASE_INSENSITIVE);
+  public static final Pattern AUTO_LINK_HASHTAGS = Pattern.compile("(^|[^0-9A-Z&/]+)(#|\uFF03)(" + HASHTAG_ALPHA_NUMERIC + "*" + HASHTAG_ALPHA + HASHTAG_ALPHA_NUMERIC + "*)", Pattern.CASE_INSENSITIVE);
   public static final int AUTO_LINK_HASHTAGS_GROUP_BEFORE = 1;
   public static final int AUTO_LINK_HASHTAGS_GROUP_HASH = 2;
   public static final int AUTO_LINK_HASHTAGS_GROUP_TAG = 3;
