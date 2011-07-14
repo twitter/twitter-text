87d2882917b9663a4d18ec437b85ced3264c26b1
keita <keita@twitter.com>
2011-07-14 13:42:08 -0700

    fix a bug in HASHTAG_ALPHA_CHARS:Cyrillic. Add Chinese iteration mark into HASHTAG_ALPHA_CHARS.

--- src/com/twitter/Regex.java
+++ src/com/twitter/Regex.java
@@ -12,13 +12,14 @@ public class Regex {
 
   private static String LATIN_ACCENTS_CHARS = "\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff";
   private static final String HASHTAG_ALPHA_CHARS = "a-z" + LATIN_ACCENTS_CHARS +
-                                                   "\\u0400-\\u04ff0\\u0500-\\u0527" + // Cyrillic
+                                                   "\\u0400-\\u04ff\\u0500-\\u0527" +  // Cyrillic
                                                    "\\u1100-\\u11ff\\u3130-\\u3185\\uA960-\\uA97F\\uAC00-\\uD7AF\\uD7B0-\\uD7FF" + // Hangul (Korean)
-                                                   "\\p{InHiragana}\\p{InKatakana}" + // Japanese Hiragana and Katakana
-                                                   "\\p{InCJKUnifiedIdeographs}\\u3005" + // Japanese Kanji / Chinese Han
-                                                   "\\uff21-\\uff3a\\uff41-\\uff5a" + // full width Alphabet
-                                                   "\\uff66-\\uff9f" + // half width Katakana
-                                                   "\\uffa1-\\uffdc"; // half width Hangul (Korean)
+                                                   "\\p{InHiragana}\\p{InKatakana}" +  // Japanese Hiragana and Katakana
+                                                   "\\p{InCJKUnifiedIdeographs}" +     // Japanese Kanji / Chinese Han
+                                                   "\\u3005\\u303b" +                  // Kanji/Han iteration marks
+                                                   "\\uff21-\\uff3a\\uff41-\\uff5a" +  // full width Alphabet
+                                                   "\\uff66-\\uff9f" +                 // half width Katakana
+                                                   "\\uffa1-\\uffdc";                  // half width Hangul (Korean)
   private static final String HASHTAG_ALPHA_NUMERIC_CHARS = "0-9\\uff10-\\uff19_" + HASHTAG_ALPHA_CHARS;
   private static final String HASHTAG_ALPHA = "[" + HASHTAG_ALPHA_CHARS +"]";
   private static final String HASHTAG_ALPHA_NUMERIC = "[" + HASHTAG_ALPHA_NUMERIC_CHARS +"]";
