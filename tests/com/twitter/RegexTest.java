e48d0f6caa1de5ee158f4431080d1cd5ceab8ba5
Katsuya Noguchi <katsuya@twitter.com>
2012-01-30 17:54:59 -0800

    Extract mention slightly more strictly

--- tests/com/twitter/RegexTest.java
+++ tests/com/twitter/RegexTest.java
@@ -85,6 +85,13 @@ public class RegexTest extends TestCase {
 
   public void testExtractMentions() {
     assertCaptureCount(2, Regex.EXTRACT_MENTIONS, "sample @user mention");
+    assertFalse("Failed to ignore unintended mention precedented by !", Regex.EXTRACT_MENTIONS.matcher("f!@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by @", Regex.EXTRACT_MENTIONS.matcher("f@@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by #", Regex.EXTRACT_MENTIONS.matcher("f#@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by $", Regex.EXTRACT_MENTIONS.matcher("f$@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by %", Regex.EXTRACT_MENTIONS.matcher("f%@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by &", Regex.EXTRACT_MENTIONS.matcher("f&@kn").find());
+    assertFalse("Failed to ignore unintended mention precedented by *", Regex.EXTRACT_MENTIONS.matcher("f*@kn").find());
   }
 
   public void testExtractReply() {
