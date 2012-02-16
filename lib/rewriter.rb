4f7c237de15061b63f77b1efc319cdccce98a7ea
Yoshimasa Niwa <niw@niw.at>
2012-02-15 21:21:21 -0800

    Deprecate some methods.

--- lib/rewriter.rb
+++ lib/rewriter.rb
@@ -1,12 +1,6 @@
 module Twitter
   # A module provides base methods to rewrite usernames, lists, hashtags and URLs.
   module Rewriter extend self
-    def rewrite(text, options = {})
-      [:hashtags, :urls, :usernames_or_lists].inject(text) do |key|
-        send("rewrite_#{key}", text, &options[key]) if options[key]
-      end
-    end
-
     def rewrite_entities(text, entities)
       begin_index = 0
       result = ""
@@ -22,6 +16,16 @@ module Twitter
       result
     end
 
+    # These methods are deprecated, will be removed in future.
+    extend Deprecation
+
+    def rewrite(text, options = {})
+      [:hashtags, :urls, :usernames_or_lists].inject(text) do |key|
+        send("rewrite_#{key}", text, &options[key]) if options[key]
+      end
+    end
+    deprecate :rewrite, :rewrite_entities
+
     def rewrite_usernames_or_lists(text)
       rewrite_entities(text, Extractor.extract_mentions_or_lists_with_indices(text)) do |entity, chars|
         at = chars[entity[:indices].first]
@@ -30,6 +34,7 @@ module Twitter
         yield(at, entity[:screen_name], list_slug) if block_given?
       end
     end
+    deprecate :rewrite_usernames_or_lists, :rewrite_entties
 
     def rewrite_hashtags(text)
       rewrite_entities(text, Extractor.extract_hashtags_with_indices(text)) do |entity, chars|
@@ -37,6 +42,7 @@ module Twitter
         yield(hash, entity[:hashtag]) if block_given?
       end
     end
+    deprecate :rewrite_hashtags, :rewrite_entties
 
     def rewrite_urls(text)
       urls = Extractor.extract_urls_with_indices(text, {:extract_url_without_protocol=>false})
@@ -44,5 +50,6 @@ module Twitter
         yield(entity[:url]) if block_given?
       end
     end
+    deprecate :rewrite_urls, :rewrite_entties
   end
 end
