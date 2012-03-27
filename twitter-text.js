1d0a93af004ecd37eaa5b5bf64ccb2386152451c
keita <keita@twitter.com>
2012-03-27 15:45:50 -0700

    Rename htmlAttrForOptions() to extractHtmlAttrsFromOptions()

--- twitter-text.js
+++ twitter-text.js
@@ -531,13 +531,12 @@ if (typeof twttr === "undefined" || twttr === null) {
     options.urlClass = options.urlClass || DEFAULT_URL_CLASS;
     options.hashtagClass = options.hashtagClass || DEFAULT_HASHTAG_CLASS;
     options.hashtagUrlBase = options.hashtagUrlBase || "https://twitter.com/#!/search?q=%23";
-    options.urlClass = options.urlClass || DEFAULT_URL_CLASS;
     options.listClass = options.listClass || DEFAULT_LIST_CLASS;
     options.usernameClass = options.usernameClass || DEFAULT_USERNAME_CLASS;
     options.usernameUrlBase = options.usernameUrlBase || "https://twitter.com/";
     options.listUrlBase = options.listUrlBase || "https://twitter.com/";
     options.before = options.before || "";
-    options.htmlAttrs = twttr.txt.htmlAttrForOptions(options);
+    options.htmlAttrs = twttr.txt.extractHtmlAttrsFromOptions(options);
 
     // remap url entities to hash
     var urlEntities, i, len;
@@ -569,7 +568,7 @@ if (typeof twttr === "undefined" || twttr === null) {
     return result;
   };
 
-  twttr.txt.htmlAttrForOptions = function(options) {
+  twttr.txt.extractHtmlAttrsFromOptions = function(options) {
     var htmlAttrs = "";
     for (var k in options) {
       var v = options[k];
