454a0f5b084a86b6b7c53139719c2a6035964fef
Kirill Lashuk <klashuk@twitter.com>
2013-11-12 16:36:33 -0800

    Allow only one nested level of balanced parentheses.

--- lib/twitter-text/regex.rb
+++ lib/twitter-text/regex.rb
@@ -221,10 +221,26 @@ module Twitter
     REGEXEN[:valid_port_number] = /[0-9]+/
 
     REGEXEN[:valid_general_url_path_chars] = /[a-z0-9!\*';:=\+\,\.\$\/%#\[\]\-_~&|@#{LATIN_ACCENTS}]/io
-    # Allow URL paths to contain balanced parens
+    # Allow URL paths to contain up to two nested levels of balanced parens
     #  1. Used in Wikipedia URLs like /Primer_(film)
     #  2. Used in IIS sessions like /S(dfd346)/
-    REGEXEN[:valid_url_balanced_parens] = /\((?:#{REGEXEN[:valid_general_url_path_chars]}|[()])+\)/io
+    #  3. Used in Rdio URLs like /track/We_Up_(Album_Version_(Edited))/
+    REGEXEN[:valid_url_balanced_parens] = /
+      \(
+        (?:
+          #{REGEXEN[:valid_general_url_path_chars]}+
+          |
+          # allow one nested level of balanced parentheses
+          (?:
+            #{REGEXEN[:valid_general_url_path_chars]}*
+            \(
+              #{REGEXEN[:valid_general_url_path_chars]}+
+            \)
+            #{REGEXEN[:valid_general_url_path_chars]}*
+          )
+        )
+      \)
+    /iox
     # Valid end-of-path chracters (so /foo. does not gobble the period).
     #   1. Allow =&# for empty URL parameters and other URL-join artifacts
     REGEXEN[:valid_url_path_ending_chars] = /[a-z0-9=_#\/\+\-#{LATIN_ACCENTS}]|(?:#{REGEXEN[:valid_url_balanced_parens]})/io
