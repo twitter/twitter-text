408c69d73462fea0c4ff08f4ed2084d33ade485b
Eden Golshani <egolshani@twitter.com>
2014-12-13 20:38:39 -0800

    updated hashtag regexes to accept hashtags in all unicode scripts, adding new tests

--- java/src/com/twitter/Regex.java
+++ java/src/com/twitter/Regex.java
@@ -59,28 +59,23 @@ public class Regex {
                                               "\\u02bb" + // Hawaiian
                                               "\\u0300-\\u036f" + // Combining diacritics
                                               "\\u1e00-\\u1eff"; // Latin Extended Additional (mostly for Vietnamese)
-  private static final String HASHTAG_ALPHA_CHARS = "a-z" + LATIN_ACCENTS_CHARS +
-                                                   "\\u0400-\\u04ff\\u0500-\\u0527" +  // Cyrillic
-                                                   "\\u2de0-\\u2dff\\ua640-\\ua69f" +  // Cyrillic Extended A/B
-                                                   "\\u0591-\\u05bf\\u05c1-\\u05c2\\u05c4-\\u05c5\\u05c7" +
-                                                   "\\u05d0-\\u05ea\\u05f0-\\u05f4" + // Hebrew
-                                                   "\\ufb1d-\\ufb28\\ufb2a-\\ufb36\\ufb38-\\ufb3c\\ufb3e\\ufb40-\\ufb41" +
-                                                   "\\ufb43-\\ufb44\\ufb46-\\ufb4f" + // Hebrew Pres. Forms
-                                                   "\\u0610-\\u061a\\u0620-\\u065f\\u066e-\\u06d3\\u06d5-\\u06dc" +
-                                                   "\\u06de-\\u06e8\\u06ea-\\u06ef\\u06fa-\\u06fc\\u06ff" + // Arabic
-                                                   "\\u0750-\\u077f\\u08a0\\u08a2-\\u08ac\\u08e4-\\u08fe" + // Arabic Supplement and Extended A
-                                                   "\\ufb50-\\ufbb1\\ufbd3-\\ufd3d\\ufd50-\\ufd8f\\ufd92-\\ufdc7\\ufdf0-\\ufdfb" + // Pres. Forms A
-                                                   "\\ufe70-\\ufe74\\ufe76-\\ufefc" + // Pres. Forms B
-                                                   "\\u200c" +                        // Zero-Width Non-Joiner
-                                                   "\\u0e01-\\u0e3a\\u0e40-\\u0e4e" + // Thai
-                                                   "\\u1100-\\u11ff\\u3130-\\u3185\\uA960-\\uA97F\\uAC00-\\uD7AF\\uD7B0-\\uD7FF" + // Hangul (Korean)
-                                                   "\\p{InHiragana}\\p{InKatakana}" +  // Japanese Hiragana and Katakana
-                                                   "\\p{InCJKUnifiedIdeographs}" +     // Japanese Kanji / Chinese Han
-                                                   "\\u3003\\u3005\\u303b" +           // Kanji/Han iteration marks
-                                                   "\\uff21-\\uff3a\\uff41-\\uff5a" +  // full width Alphabet
-                                                   "\\uff66-\\uff9f" +                 // half width Katakana
-                                                   "\\uffa1-\\uffdc";                  // half width Hangul (Korean)
-  private static final String HASHTAG_ALPHA_NUMERIC_CHARS = "0-9\\uff10-\\uff19_" + HASHTAG_ALPHA_CHARS;
+  private static final String HASHTAG_ALPHA_CHARS = "\\p{L}\\p{M}";
+  private static final String HASHTAG_NUMERALS = "\\p{Nd}";
+  private static final String HASHTAG_SPECIAL_CHARS = "_" + //underscore
+                                                      "\\u200c" + // ZERO WIDTH NON-JOINER (ZWNJ)
+                                                      "\\ua67e" + // CYRILLIC KAVYKA
+                                                      "\\u05be" + // HEBREW PUNCTUATION MAQAF
+                                                      "\\u05f3" + // HEBREW PUNCTUATION GERESH
+                                                      "\\u05f4" + // HEBREW PUNCTUATION GERSHAYIM
+                                                      "\\u309b" + // KATAKANA-HIRAGANA VOICED SOUND MARK
+                                                      "\\u309c" + // KATAKANA-HIRAGANA SEMI-VOICED SOUND MARK
+                                                      "\\u30a0" + // KATAKANA-HIRAGANA DOUBLE HYPHEN
+                                                      "\\u30fb" + // KATAKANA MIDDLE DOT
+                                                      "\\u3003" + // DITTO MARK
+                                                      "\\u0f0b" + // TIBETAN MARK INTERSYLLABIC TSHEG
+                                                      "\\u0f0c" + // TIBETAN MARK DELIMITER TSHEG BSTAR
+                                                      "\\u0f0d";  // TIBETAN MARK SHAD
+  private static final String HASHTAG_ALPHA_NUMERIC_CHARS = HASHTAG_ALPHA_CHARS + HASHTAG_NUMERALS + HASHTAG_SPECIAL_CHARS;
   private static final String HASHTAG_ALPHA = "[" + HASHTAG_ALPHA_CHARS +"]";
   private static final String HASHTAG_ALPHA_NUMERIC = "[" + HASHTAG_ALPHA_NUMERIC_CHARS +"]";
 
--- rb/lib/twitter-text/regex.rb
+++ rb/lib/twitter-text/regex.rb
@@ -90,6 +90,7 @@ module Twitter
           regex_range(0x0300, 0x036f),
           regex_range(0x1e00, 0x1eff)
     ].join('').freeze
+    REGEXEN[:latin_accents] = /[#{LATIN_ACCENTS}]+/o
 
     RTL_CHARACTERS = [
       regex_range(0x0600,0x06FF),
@@ -98,78 +99,14 @@ module Twitter
       regex_range(0xFE70,0xFEFF)
     ].join('').freeze
 
-
-    NON_LATIN_HASHTAG_CHARS = [
-      # Cyrillic (Russian, Ukrainian, etc.)
-      regex_range(0x0400, 0x04ff), # Cyrillic
-      regex_range(0x0500, 0x0527), # Cyrillic Supplement
-      regex_range(0x2de0, 0x2dff), # Cyrillic Extended A
-      regex_range(0xa640, 0xa69f), # Cyrillic Extended B
-      regex_range(0x0591, 0x05bf), # Hebrew
-      regex_range(0x05c1, 0x05c2),
-      regex_range(0x05c4, 0x05c5),
-      regex_range(0x05c7),
-      regex_range(0x05d0, 0x05ea),
-      regex_range(0x05f0, 0x05f4),
-      regex_range(0xfb12, 0xfb28), # Hebrew Presentation Forms
-      regex_range(0xfb2a, 0xfb36),
-      regex_range(0xfb38, 0xfb3c),
-      regex_range(0xfb3e),
-      regex_range(0xfb40, 0xfb41),
-      regex_range(0xfb43, 0xfb44),
-      regex_range(0xfb46, 0xfb4f),
-      regex_range(0x0610, 0x061a), # Arabic
-      regex_range(0x0620, 0x065f),
-      regex_range(0x066e, 0x06d3),
-      regex_range(0x06d5, 0x06dc),
-      regex_range(0x06de, 0x06e8),
-      regex_range(0x06ea, 0x06ef),
-      regex_range(0x06fa, 0x06fc),
-      regex_range(0x06ff),
-      regex_range(0x0750, 0x077f), # Arabic Supplement
-      regex_range(0x08a0),         # Arabic Extended A
-      regex_range(0x08a2, 0x08ac),
-      regex_range(0x08e4, 0x08fe),
-      regex_range(0xfb50, 0xfbb1), # Arabic Pres. Forms A
-      regex_range(0xfbd3, 0xfd3d),
-      regex_range(0xfd50, 0xfd8f),
-      regex_range(0xfd92, 0xfdc7),
-      regex_range(0xfdf0, 0xfdfb),
-      regex_range(0xfe70, 0xfe74), # Arabic Pres. Forms B
-      regex_range(0xfe76, 0xfefc),
-      regex_range(0x200c, 0x200c), # Zero-Width Non-Joiner
-      regex_range(0x0e01, 0x0e3a), # Thai
-      regex_range(0x0e40, 0x0e4e), # Hangul (Korean)
-      regex_range(0x1100, 0x11ff), # Hangul Jamo
-      regex_range(0x3130, 0x3185), # Hangul Compatibility Jamo
-      regex_range(0xA960, 0xA97F), # Hangul Jamo Extended-A
-      regex_range(0xAC00, 0xD7AF), # Hangul Syllables
-      regex_range(0xD7B0, 0xD7FF), # Hangul Jamo Extended-B
-      regex_range(0xFFA1, 0xFFDC)  # Half-width Hangul
-    ].join('').freeze
-    REGEXEN[:latin_accents] = /[#{LATIN_ACCENTS}]+/o
-
-    CJ_HASHTAG_CHARACTERS = [
-      regex_range(0x30A1, 0x30FA), regex_range(0x30FC, 0x30FE), # Katakana (full-width)
-      regex_range(0xFF66, 0xFF9F), # Katakana (half-width)
-      regex_range(0xFF10, 0xFF19), regex_range(0xFF21, 0xFF3A), regex_range(0xFF41, 0xFF5A), # Latin (full-width)
-      regex_range(0x3041, 0x3096), regex_range(0x3099, 0x309E), # Hiragana
-      regex_range(0x3400, 0x4DBF), # Kanji (CJK Extension A)
-      regex_range(0x4E00, 0x9FFF), # Kanji (Unified)
-      regex_range(0x20000, 0x2A6DF), # Kanji (CJK Extension B)
-      regex_range(0x2A700, 0x2B73F), # Kanji (CJK Extension C)
-      regex_range(0x2B740, 0x2B81F), # Kanji (CJK Extension D)
-      regex_range(0x2F800, 0x2FA1F), regex_range(0x3003), regex_range(0x3005), regex_range(0x303B) # Kanji (CJK supplement)
-    ].join('').freeze
-
     PUNCTUATION_CHARS = '!"#$%&\'()*+,-./:;<=>?@\[\]^_\`{|}~'
     SPACE_CHARS = " \t\n\x0B\f\r"
     CTRL_CHARS = "\x00-\x1F\x7F"
 
-    # A hashtag must contain latin characters, numbers and underscores, but not all numbers.
-    HASHTAG_ALPHA = /[a-z_#{LATIN_ACCENTS}#{NON_LATIN_HASHTAG_CHARS}#{CJ_HASHTAG_CHARACTERS}]/io
-    HASHTAG_ALPHANUMERIC = /[a-z0-9_#{LATIN_ACCENTS}#{NON_LATIN_HASHTAG_CHARS}#{CJ_HASHTAG_CHARACTERS}]/io
-    HASHTAG_BOUNDARY = /\A|\z|[^&a-z0-9_#{LATIN_ACCENTS}#{NON_LATIN_HASHTAG_CHARS}#{CJ_HASHTAG_CHARACTERS}]/o
+    # A hashtag must contain at least one unicode letter or mark, as well as numbers, underscores, and select special characters.
+    HASHTAG_ALPHA = /[\p{L}\p{M}]/
+    HASHTAG_ALPHANUMERIC = /[\p{L}\p{M}\p{Nd}_\u200c\u0482\ua673\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u0f0d]/
+    HASHTAG_BOUNDARY = /\A|\z|[^&\p{L}\p{M}\p{Nd}_\u200c\u0482\ua673\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u0f0d]/
 
     HASHTAG = /(#{HASHTAG_BOUNDARY})(#|＃)(#{HASHTAG_ALPHANUMERIC}*#{HASHTAG_ALPHA}#{HASHTAG_ALPHANUMERIC}*)/io
 
