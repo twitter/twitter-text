d60059262eb7cf361ac8aebaafb13c5c1e6dc828
Catia Goncalves <cgoncalves@twitter.com>
2016-12-05 15:09:55 -0800

    reverting cashtags for now to proceed with tld update

--- js/twitter-text.js
+++ js/twitter-text.js
@@ -270,7 +270,7 @@
   twttr.txt.regexen.urlHasHttps = /^https:\/\//i;
 
   // cashtag related regex
-  twttr.txt.regexen.cashtag = /(?:[a-z]{1,2}[:._-][0-9]{1,6})|(?:[a-z]{1,6}(?:[:._-](?:[a-z]|[0-9]){1,6})?)|(?:[0-9]{1,6}[:._-][a-z]{1,2})/i;
+  twttr.txt.regexen.cashtag = /[a-z]{1,6}(?:[._][a-z]{1,2})?/i;
   twttr.txt.regexen.validCashtag = regexSupplant('(^|#{spaces})(\\$)(#{cashtag})(?=$|\\s|[#{punct}])', 'gi');
 
   // These URL validation pattern strings are based on the ABNF from RFC 3986
