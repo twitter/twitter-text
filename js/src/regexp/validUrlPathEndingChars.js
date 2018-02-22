cadb9b22aa9f4bcd40e2d5754c47d4fe8d667838
tparsioan <tparsioan@users.noreply.github.com>
2018-02-22 17:42:24 -0500

    Issue #254: support exclamation point as ending char in path

--- js/src/regexp/validUrlPathEndingChars.js
+++ js/src/regexp/validUrlPathEndingChars.js
@@ -6,7 +6,7 @@ import validUrlBalancedParens from './validUrlBalancedParens';
 // Valid end-of-path chracters (so /foo. does not gobble the period).
 // 1. Allow =&# for empty URL parameters and other URL-join artifacts
 const validUrlPathEndingChars = regexSupplant(
-  /[\+\-a-z#{cyrillicLettersAndMarks}0-9=_#\/#{latinAccentChars}]|(?:#{validUrlBalancedParens})/i,
+  /[\+\-a-z#{cyrillicLettersAndMarks}0-9=_#!\/#{latinAccentChars}]|(?:#{validUrlBalancedParens})/i,
   { cyrillicLettersAndMarks, latinAccentChars, validUrlBalancedParens }
 );
 
