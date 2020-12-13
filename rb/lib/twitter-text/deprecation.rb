0179a27e5d4dfe18343bcb1e95c2ab019eb20c6c
Maxwell Salzberg <maxwell@backerkit.com>
2020-12-13 13:43:11 -0800

    Update Deprecation method to allow for setting frozen_string_literal: true

--- rb/lib/twitter-text/deprecation.rb
+++ rb/lib/twitter-text/deprecation.rb
@@ -8,7 +8,7 @@ module Twitter
       def deprecate(method, new_method = nil)
         deprecated_method = :"deprecated_#{method}"
         message = "Deprecation: `#{method}` is deprecated."
-        message << " Please use `#{new_method}` instead." if new_method
+        message += " Please use `#{new_method}` instead." if new_method
 
         alias_method(deprecated_method, method)
         define_method method do |*args, &block|
