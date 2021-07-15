d28b637f28b840bc172b5f870cff73819ee29779
Michael Glenn <mbg198618@gmail.com>
2021-07-15 10:51:22 -0500

    Create twitter-API.js

---
+++ twitter-API.js
@@ -0,0 +1,83 @@
+### Twitter-API
+---
+Getting started
+About the Twitter API
+Getting access to the Twitter API
+Important resources
+About the Twitter API
+The Twitter API can be used to programmatically retrieve and analyze data, as well as engage with the conversation on Twitter.
+---
+# This API provides access to a variety of different resources including the following:
+>
+-Tweets
+-Users
+-Direct Messages
+-Lists
+-Trends
+-Media
+-Places
+ >
+# The Twitter API currently consists of two supported versions, as well as different access tiers.
+We have listed these below to help you better understand which version and tiers are available, 
+but would recommend using the newest versions
+(Twitter API v2) 
+where available.
+Learn more about our new versioning strategy.
+---
+Version / tier	Description
+Twitter API v2: Early Access	
+We are providing Early Access to our new v2 endpoints, which include the following:
+---
+## Lookup Tweets and lookup users
+Search for recent Tweets, or for Tweets from the entire archive of public Tweets
+Retrieve Tweet counts that match a query in the last 7 days or from the entire archive
+Filter or Sample real-time streaming Tweets
+Pull user Tweet and mentions timelines
+Understand and manage liking and unliking of Tweets
+Understand and manage user following relationships
+Understand and manage blocking and unblocking user relationships
+Pull new metrics
+Receive new Tweet annotations fields in responses, and filter search queries based on this data 
+Receive standardized JSON data objects with just the fields that you need
+And much more
+> 
+# Standard v1.1	
+-The legacy standard endpoints provide access to the following resources with the standard v1.1 offerings.
+---
+Post and engage with Tweets
+Retrieve timelines
+Post and receive direct messages
+Manage and pull public account information
+Create and manage lists
+Follow, search, and get users
+Retrieve trends
+---
+# Premium v1.1	
+-Scaled access to Search Tweets and our Account Activity API,
+Including a free sandbox tier and multiple paid tiers.
+---
+>
+# Enterprise	
+-Enterprise-level products that provide access to Twitter’s data, 
+-including Full Archive and 30 Day Search, PowerTrack, 
+-Historical PowerTrack, 
+-Decahose, 
+-Engagement API,
+-and much more!
+--- 
+>
+> 
+>
+# We will eventually release Twitter API v2
+equivalents for most of the currently available endpoints. 
+To learn more about Twitter API v2:  Early Access, 
+visit one of the following resources:
+---
+# Migration hub for 
+how to upgrade your current v1.1 endpoints to v2
+# Guide to the future of the Twitter API
+Tutorials on how to build with Twitter API v2
+---
+# Please stay informed to receive updates about our roadmap,
+and let us know what you'd like to see on our roadmap.
+---
